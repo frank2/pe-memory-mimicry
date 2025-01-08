fd0939ec1fbc026f9733c566eca3b3e6eebf76b4
frank2 <frank2@dc949.org>
2025-01-07 21:27:30 -0800

    manually acquire LoadLibraryA and VirtualProtect to import our libs

--- sheep_monitor/main.cpp
+++ sheep_monitor/main.cpp
@@ -13,6 +13,104 @@
 
 #define VA_TO_RVA(base, va) (((std::uintptr_t)va) - ((std::uintptr_t)base))
 
+typedef PVOID PACTIVATION_CONTEXT;
+
+typedef struct _LDR_DATA_TABLE_ENTRY_EX {
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderLinks;
+	LIST_ENTRY InInitializationOrderLinks;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+	ULONG Flags;
+	WORD LoadCount;
+	WORD TlsIndex;
+	union {
+		LIST_ENTRY HashLinks;
+		struct {
+			PVOID SectionPointer;
+			ULONG CheckSum;
+		};
+	};
+	union {
+		ULONG TimeDateStamp;
+		PVOID LoadedImports;
+	};
+	PACTIVATION_CONTEXT EntryPointActivationContext;
+	PVOID PatchInformation;
+	LIST_ENTRY ForwarderLinks;
+	LIST_ENTRY ServiceTagLinks;
+	LIST_ENTRY StaticLinks;
+} LDR_DATA_TABLE_ENTRY_EX, * PLDR_DATA_TABLE_ENTRY_EX;
+
+typedef struct _PEB_LDR_DATA_EX {
+   ULONG                   Length;
+   ULONG                   Initialized;
+   PVOID                   SsHandle;
+   LIST_ENTRY              InLoadOrderModuleList;
+   LIST_ENTRY              InMemoryOrderModuleList;
+   LIST_ENTRY              InInitializationOrderModuleList;
+} PEB_LDR_DATA_EX, * PPEB_LDR_DATA_EX;
+
+typedef struct _PEB_EX {
+	BOOLEAN                 InheritedAddressSpace;
+	BOOLEAN                 ReadImageFileExecOptions;
+	BOOLEAN                 BeingDebugged;
+	BOOLEAN                 Spare;
+	HANDLE                  Mutant;
+	PVOID                   ImageBase;
+	PPEB_LDR_DATA_EX        LoaderData;
+	PVOID                   ProcessParameters;
+	PVOID                   SubSystemData;
+	PVOID                   ProcessHeap;
+	PVOID                   FastPebLock;
+	PVOID                   FastPebLockRoutine;
+	PVOID                   FastPebUnlockRoutine;
+	ULONG                   EnvironmentUpdateCount;
+	PVOID* KernelCallbackTable;
+	PVOID                   EventLogSection;
+	PVOID                   EventLog;
+	PVOID                   FreeList;
+	ULONG                   TlsExpansionCounter;
+	PVOID                   TlsBitmap;
+	ULONG                   TlsBitmapBits[0x2];
+	PVOID                   ReadOnlySharedMemoryBase;
+	PVOID                   ReadOnlySharedMemoryHeap;
+	PVOID* ReadOnlyStaticServerData;
+	PVOID                   AnsiCodePageData;
+	PVOID                   OemCodePageData;
+	PVOID                   UnicodeCaseTableData;
+	ULONG                   NumberOfProcessors;
+	ULONG                   NtGlobalFlag;
+	BYTE                    Spare2[0x4];
+	LARGE_INTEGER           CriticalSectionTimeout;
+	ULONG                   HeapSegmentReserve;
+	ULONG                   HeapSegmentCommit;
+	ULONG                   HeapDeCommitTotalFreeThreshold;
+	ULONG                   HeapDeCommitFreeBlockThreshold;
+	ULONG                   NumberOfHeaps;
+	ULONG                   MaximumNumberOfHeaps;
+	PVOID** ProcessHeaps;
+	PVOID                   GdiSharedHandleTable;
+	PVOID                   ProcessStarterHelper;
+	PVOID                   GdiDCAttributeList;
+	PVOID                   LoaderLock;
+	ULONG                   OSMajorVersion;
+	ULONG                   OSMinorVersion;
+	ULONG                   OSBuildNumber;
+	ULONG                   OSPlatformId;
+	ULONG                   ImageSubSystem;
+	ULONG                   ImageSubSystemMajorVersion;
+	ULONG                   ImageSubSystemMinorVersion;
+	ULONG                   GdiHandleBuffer[0x22];
+	ULONG                   PostProcessInitRoutine;
+	ULONG                   TlsExpansionBitmap;
+	BYTE                    TlsExpansionBitmapBits[0x80];
+	ULONG                   SessionId;
+} PEB_EX, * PPEB_EX;
+
 void exit_thread(void) {
    ExitThread(0);
 }
@@ -29,7 +127,61 @@ struct SheepConfig {
 
 SheepConfig *GLOBAL_CONFIG = nullptr;
 
+PIMAGE_NT_HEADERS64 get_nt_headers(uint8_t *base) {
+   PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)base;
+   return (PIMAGE_NT_HEADERS64)&base[dos_header->e_lfanew];
+}
+
+uint8_t *get_proc_address(uint8_t *module, const char *export) {
+   PIMAGE_NT_HEADERS64 nt_headers = get_nt_headers(module);
+   PIMAGE_DATA_DIRECTORY export_dir_info = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+   PIMAGE_EXPORT_DIRECTORY export_dir = (PIMAGE_EXPORT_DIRECTORY)&image_base[export_dir_info->VirtualAddress];
+
+   DWORD *functions = (DWORD *)&image_base[export_dir->AddressOfFunctions];
+   DWORD *names = (DWORD *)&image_base[export_dir->AddressOfNames];
+   WORD *name_ordinals = (WORD *)&image_base[export_dir->AddressOfNameOrdinals];
+
+   for (size_t i=0; i<export_dir->NumberOfNames; ++i) {
+      const char *target_export = (const char *)&image_base[names[i]];
+      const char *export_copy = export;
+      bool found = true;
+
+      while (*export_copy != 0 || *target_export != 0) {
+         if (*export_copy == 0 && *target_export != 0 || *target_export == 0 && *export_copy != 0) {
+            found = false;
+            break;
+         }
+
+         std::int8_t diff = *target_export - *export_copy;
+
+         if (diff != 0) {
+            found = false;
+            break;
+         }
+
+         ++target_export;
+         ++export_copy;
+      }
+
+      if (found == false)
+         continue;
+         
+      return &module[functions[name_ordinals[i]]];
+   }
+
+   return NULL;
+}
+   
 extern "C" __declspec(dllexport) DWORD WINAPI load_image(SheepConfig *config) {
+   PPEB_EX peb_ex = ((PPEB_EX)__readgsqword(0x60));
+   PPEB_LDR_DATA_EX ldr_ex = (PPEB_LDR_DATA_EX)peb_ex->LoaderData;
+   PLDR_DATA_TABLE_ENTRY_EX list_entry = (PLDR_DATA_TABLE_ENTRY_EX)ldr_ex->InLoadOrderModuleList.Flink;
+   PLDR_DATA_TABLE_ENTRY_EX ntdll_entry = (PLDR_DATA_TABLE_ENTRY_EX)list_entry->InLoadOrderLinks.Flink;
+   PLDR_DATA_TABLE_ENTRY_EX kernel32_entry = (PLDR_DATA_TABLE_ENTRY_EX)ntdll_entry->InLoadOrderLinks.Flink;
+
+   std::uint8_t * (*load_library)(const char *) = (std::uint8_t * (*)(const char *))get_proc_address((std::uint8_t *)kernel32_entry->DllBase, "LoadLibraryA");
+   BOOL (*virtual_protect)(LPVOID, SIZE_T, DWORD, PDWORD) = (BOOL (*)(LPVOID, SIZE_T, DWORD, PDWORD))get_proc_address((std::uint8_t *)kernel32_entry->DllBase, "VirtualProtect");
+   
    GLOBAL_CONFIG = config;
    std::uint8_t *base_u8 = (std::uint8_t *)GLOBAL_CONFIG->image_base;
    PIMAGE_NT_HEADERS64 base_nt = get_nt_headers(base_u8);
@@ -40,21 +192,26 @@ extern "C" __declspec(dllexport) DWORD WINAPI load_image(SheepConfig *config) {
       PIMAGE_IMPORT_DESCRIPTOR import_table = (PIMAGE_IMPORT_DESCRIPTOR)&base_u8[import_rva];
 
       while (import_table->OriginalFirstThunk != 0) {
-         HMODULE module = LoadLibraryA((const char *)&base_u8[import_table->Name]);
+         std::uint8_t *module = load_library((const char *)&base_u8[import_table->Name]);
          std::uintptr_t *original_thunks = (std::uintptr_t *)&base_u8[import_table->OriginalFirstThunk];
          std::uintptr_t *import_addrs = (std::uintptr_t *)&base_u8[import_table->FirstThunk];
+         std::uintptr_t *old_base = import_addrs;
+         DWORD old_protect;
+         DWORD new_protect = PAGE_READWRITE;
+         virtual_protect(import_addrs, 1024, new_protect, &old_protect);
 
          while (*original_thunks != 0) {
             if (*original_thunks & 0x8000000000000000)
-               *import_addrs = (std::uintptr_t)GetProcAddress(module, MAKEINTRESOURCE(*original_thunks & 0xFFFF));
+               *import_addrs = (std::uintptr_t)get_proc_address(module, MAKEINTRESOURCE(*original_thunks & 0xFFFF));
             else {
                PIMAGE_IMPORT_BY_NAME import_by_name = (PIMAGE_IMPORT_BY_NAME)&base_u8[*original_thunks];
-               *import_addrs = (std::uintptr_t)GetProcAddress(module, import_by_name->Name);
+               *import_addrs = (std::uintptr_t)get_proc_address(module, import_by_name->Name);
             }
-
             ++import_addrs;
             ++original_thunks;
          }
+
+         virtual_protect(old_base, 1024, old_protect, &new_protect);
 
          ++import_table;
       }
